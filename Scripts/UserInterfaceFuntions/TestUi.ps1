fe852d6d323bee72f5bc61ea0ebee0ba58a04b9c
Nutellafreak96 <66807184+Nutellafreak96@users.noreply.github.com>
2025-07-14 10:43:20 +0200

    add TestUi.ps1 - to test the userinterface functions

---
+++ Scripts/UserInterfaceFuntions/TestUi.ps1
@@ -0,0 +1,12 @@
+
+<#Dot-Sourcing to use the functions of the UiFunctions.ps1 File#>
+. .\UiFunctions.ps1
+
+FirmName #get input from the user about the firm name
+DomainValueUI #get input from the user for important domain values
+SwitchSelectorUI #let the user decide which networkswitch the vms use
+RamSelectorUI #let the user decied how much ram the vms use
+ServerIPAdressUI #get input from the user about the ip-addresses the vms should use
+SelectDirUI #let the user select the directory to store the vms
+PasswordUI # let the user input the password used by the script
+
