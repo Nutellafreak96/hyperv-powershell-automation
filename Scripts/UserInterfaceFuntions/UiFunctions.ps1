9f0974d98b2b794125ae2e23e4dc72d50e42819e
Nutellafreak96 <66807184+Nutellafreak96@users.noreply.github.com>
2025-07-14 09:14:31 +0200

    Add function DomainValueUI to  UiFunctions.ps1 - Get userInput for needed Domain Values like NetBIOS Name, Domain Name

--- Scripts/UserInterfaceFuntions/UiFunctions.ps1
+++ Scripts/UserInterfaceFuntions/UiFunctions.ps1
@@ -1,11 +1,17 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-<#Funktion zur Abfrage des Firmennamen#>
-function KundenDetails {
+<#Function to get the firm name#>
+function FirmName {
+
+    ###############################################################################
+    #                                                                             #
+    #                User Interface for the Firm name                             # 
+    #                                                                             #
+    ###############################################################################
 
     $form = New-Object System.Windows.Forms.Form
-    $form.Text = "Firmenname"
+    $form.Text = "Firm name"
     $form.Size = New-Object System.Drawing.Size(300, 150)
     $form.StartPosition = "CenterScreen"
 
@@ -28,7 +34,7 @@ function KundenDetails {
     $label = New-Object System.Windows.Forms.Label
     $label.Location = New-Object System.Drawing.Point(10, 20)
     $label.Size = New-Object System.Drawing.Size(280, 20)
-    $label.Text = "Geben Sie den Namen der Firma ein"
+    $label.Text = "Please enter a Firm name"
     $form.Controls.Add($label)
 
     $textBox = New-Object System.Windows.Forms.TextBox
@@ -45,7 +51,91 @@ function KundenDetails {
         return $textBox.Text
     }
     else {
-        Write-Host "Fehlende/Falsche Eingabe"
+        Write-Host "Wrong/Missing Input"
         Exit
     }
 }
+
+<#Function to get values ecessary for the domain/organisationalunit#>
+function DomainValueUI {
+
+    ###############################################################################
+    #                                                                             #
+    #                User Interface for the Domain Values                         # 
+    #                                                                             #
+    ###############################################################################
+
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = "Domain Values"
+    $form.Size = New-Object System.Drawing.Size(300, 250)
+    $form.StartPosition = "CenterScreen"
+
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Point(80, 173)
+    $okButton.Size = New-Object System.Drawing.Size(60, 20)
+    $okButton.Text = "OK"
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Point(140, 173)
+    $cancelButton.Size = New-Object System.Drawing.Size(60, 20)
+    $cancelButton.Text = "Cancel"
+    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $form.CancelButton = $cancelButton
+    $form.Controls.Add($cancelButton)
+
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Point(10, 20)
+    $label.Size = New-Object System.Drawing.Size(280, 20)
+    $label.Text = "Please enter a Domain Value:"
+    $form.Controls.Add($label)
+
+    $textBox = New-Object System.Windows.Forms.TextBox
+    $textBox.Location = New-Object System.Drawing.Point(10, 40)
+    $textBox.Size = New-Object System.Drawing.Size(260, 20)
+    $form.Controls.Add($textBox)
+
+    $label2 = New-Object System.Windows.Forms.Label
+    $label2.Location = New-Object System.Drawing.Point(10, 70)
+    $label2.Size = New-Object System.Drawing.Size(280, 20)
+    $label2.Text = "Please Enter a NetBIOS name:"
+    $form.Controls.Add($label2)
+
+    $textBox1 = New-Object System.Windows.Forms.TextBox
+    $textBox1.Location = New-Object System.Drawing.Point(10, 90)
+    $textBox1.Size = New-Object System.Drawing.Size(260, 20)
+    $form.Controls.Add($textBox1)
+
+    $label3 = New-Object System.Windows.Forms.Label
+    $label3.Location = New-Object System.Drawing.Point(10, 120)
+    $label3.Size = New-Object System.Drawing.Size(280, 20)
+    $label3.Text = "Please enter a Name for the Organizational Unit:"
+    $form.Controls.Add($label3)
+
+    $textBox2 = New-Object System.Windows.Forms.TextBox
+    $textBox2.Location = New-Object System.Drawing.Point(10, 140)
+    $textBox2.Size = New-Object System.Drawing.Size(260, 20)
+    $form.Controls.Add($textBox2)
+
+    $form.Topmost = $true
+
+    $form.Add_Shown({ $textBox.Select() })
+    $result = $form.ShowDialog()
+
+    $array = @()
+
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK -and ( ($null -notlike $textBox.text) -and ($null -notlike $textBox1.text) -and ($null -notlike $textBox2.text) )) {
+        $array += $textBox.Text
+        $array += $textBox1.Text
+        $array += $textBox2.Text
+
+        return $array
+    }
+    else {
+        Write-Host "Wrong/Missing Input"
+        exit
+    }
+}
+
