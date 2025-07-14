3988f3c5a93743a94aac9b5fd1810837aa08dd42
Nutellafreak96 <66807184+Nutellafreak96@users.noreply.github.com>
2025-07-14 09:35:35 +0200

    Add RamSelectorUI to UiFunctions.ps1 - let the User decide how much ram to use

--- Scripts/UserInterfaceFuntions/UiFunctions.ps1
+++ Scripts/UserInterfaceFuntions/UiFunctions.ps1
@@ -173,7 +173,7 @@ function SwitchSelectorUI {
     $label = New-Object System.Windows.Forms.Label
     $label.Location = New-Object System.Drawing.Point(10, 20)
     $label.Size = New-Object System.Drawing.Size(230, 20)
-    $label.Text = "Please entere the Switch you want to use:"
+    $label.Text = "Please select the Switch you want to use:"
     $form.Controls.Add($label)
 
     $listBox = New-Object System.Windows.Forms.ListBox
@@ -194,6 +194,85 @@ function SwitchSelectorUI {
 
     if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
         return $listBox.SelectedItem
+    }
+    else {
+        Return "Wrong/Missing Input"
+    }
+}
+
+<##>
+function RamSelectorUI {
+
+    ###############################################################################
+    #                                                                             #
+    #          User Interface for the RAM of the VMs                              # 
+    #                                                                             #
+    ###############################################################################
+
+
+    $RAMArray = @(
+        2,
+        4,
+        8,
+        12,
+        16,
+        32,
+        64) #Array fuer die RAM Zuweisung
+
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = "RAM Selector"
+    $form.Size = New-Object System.Drawing.Size(276, 220)
+    $form.StartPosition = "CenterScreen"
+
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Point(65, 150)
+    $okButton.Size = New-Object System.Drawing.Size(60, 20)
+    $okButton.Text = "OK"
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Point(125, 150)
+    $cancelButton.Size = New-Object System.Drawing.Size(60, 20)
+    $cancelButton.Text = "Cancel"
+    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $form.CancelButton = $cancelButton
+    $form.Controls.Add($cancelButton)
+
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Point(10, 20)
+    $label.Size = New-Object System.Drawing.Size(260, 20)
+    $label.Text = "Please select the RAM value to use(in MB):"
+    $form.Controls.Add($label)
+
+    $listBox = New-Object System.Windows.Forms.ListBox
+    $listBox.Location = New-Object System.Drawing.Point(10, 40)
+    $listBox.Size = New-Object System.Drawing.Size(242, 20)
+    $listBox.Height = 105
+
+    [void] $listBox.Items.AddRange($RAMArray)
+    $listBox.SelectedIndex = 0
+    $form.Add_Shown({ $listBox.Focus() })
+
+    $form.Controls.Add($listBox)
+
+    $form.Topmost = $true
+
+    $result = $form.ShowDialog()
+
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+        $RamSelected = $listBox.SelectedItem
+        switch ($RamSelected) {
+            2 { return 2GB }
+            4 { return 4GB }
+            8 { return 8GB }
+            12 { return 12GB }
+            16 { return 16GB }
+            32 { return 32GB }
+            64 { return 64GB }
+            Default { return 2GB }
+        }
     }
     else {
         Return "Wrong/Missing Input"
