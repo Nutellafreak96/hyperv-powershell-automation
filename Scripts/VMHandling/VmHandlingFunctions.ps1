349744a767a5d527d8960edd7998b4819f70c90c
Nutellafreak96 <66807184+Nutellafreak96@users.noreply.github.com>
2025-07-16 15:49:42 +0200

    Add ChangeMacAddress Function to FileHandlingFunctions.ps1 - function to set the mac of the vms in hyper v to static

--- Scripts/VMHandling/VmHandlingFunctions.ps1
+++ Scripts/VMHandling/VmHandlingFunctions.ps1
@@ -16,3 +16,21 @@ function UpdateVMRessources {
     Set-VM -Name $FS -ProcessorCount 4 -MemoryStartupBytes $RamFs| Out-Null
     Set-VM -Name $TS -ProcessorCount 4 -MemoryStartupBytes $RamTs | Out-Null
 }
+<#Set the MAC Address of the VM to static#>
+function ChangeMacAddress {
+    param(
+        [string]$DC,
+        [string]$FS,
+        [string]$TS
+    )
+    
+    #Die MacAddressen der Vms auslesen
+    $DCMacAddress = Get-VM -Name $DC | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
+    $FSMacAddress = Get-VM -Name $FS | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
+    $TSMacAddress = Get-VM -Name $TS | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
+
+    #Den VMs statische MacAddressen geben
+    Set-VMNetworkAdapter -VMName $DC -StaticMacAddress $DCMacAddress
+    Set-VMNetworkAdapter -VMName $FS -StaticMacAddress $FSMacAddress
+    Set-VMNetworkAdapter -VMName $TS -StaticMacAddress $TSMacAddress
+}
