bb4e6519ad72d48aa59a7483db218ce390f40102
Nutellafreak <huebi.kevin@gmx.de>
2025-07-19 15:26:34 +0200

    Updated variable names and added comments to VmHandlingFunctions.ps1

--- Scripts/VMHandling/VmHandlingFunctions.ps1
+++ Scripts/VMHandling/VmHandlingFunctions.ps1
@@ -1,9 +1,42 @@
-<#function to update the ressources of the vms#>
-function UpdateVMRessources {
+<#
+.SYNOPSIS
+Updates CPU and memory resources of the specified VMs.
+
+.DESCRIPTION
+Modifies the number of virtual processors and sets the startup memory for each VM (Domain Controller, File Server, Terminal Server).
+
+.PARAMETER VmDC
+Name of the Domain Controller VM.
+
+.PARAMETER VmFs
+Name of the File Server VM.
+
+.PARAMETER VmTs
+Name of the Terminal Server VM.
+
+.PARAMETER CoreDc
+Number of CPU cores for the Domain Controller.
+
+.PARAMETER CoreFs
+Number of CPU cores for the File Server.
+
+.PARAMETER CoreTs
+Number of CPU cores for the Terminal Server.
+
+.PARAMETER RamDc
+RAM allocation (in bytes) for the Domain Controller.
+
+.PARAMETER RamFs
+RAM allocation (in bytes) for the File Server.
+
+.PARAMETER RamTs
+RAM allocation (in bytes) for the Terminal Server.
+#>
+function UpdateVMResources {
     param(
-        [string]$DC,
-        [string]$FS,
-        [string]$TS,
+        [string]$VmDC,
+        [string]$VmFs,
+        [string]$VmTs,
         [Int16]$CoreDc,
         [Int16]$CoreFs,
         [Int16]$CoreTs,
@@ -11,43 +44,75 @@ function UpdateVMRessources {
         [Int64]$RamFs,
         [Int64]$RamTs
     )
-    #Bearbeiten der Anzahl der virtuellen Prozessoren
-    Set-VM -Name $DC -ProcessorCount 4 -MemoryStartupBytes $RamDc | Out-Null
-    Set-VM -Name $FS -ProcessorCount 4 -MemoryStartupBytes $RamFs| Out-Null
-    Set-VM -Name $TS -ProcessorCount 4 -MemoryStartupBytes $RamTs | Out-Null
+
+    Set-VM -Name $VmDC -ProcessorCount $CoreDc -MemoryStartupBytes $RamDc | Out-Null
+    Set-VM -Name $VmFs -ProcessorCount $CoreFs -MemoryStartupBytes $RamFs | Out-Null
+    Set-VM -Name $VmTs -ProcessorCount $CoreTs -MemoryStartupBytes $RamTs | Out-Null
 }
-<#Set the MAC Address of the VM to static#>
+
+<#
+.SYNOPSIS
+Sets the MAC address of VMs to static values.
+
+.DESCRIPTION
+Reads the current dynamic MAC address of the VMs and reassigns them as static to preserve the address.
+
+.PARAMETER VmDC
+Name of the Domain Controller VM.
+
+.PARAMETER VmFs
+Name of the File Server VM.
+
+.PARAMETER VmTs
+Name of the Terminal Server VM.
+#>
 function ChangeMacAddress {
     param(
-        [string]$DC,
-        [string]$FS,
-        [string]$TS
+        [string]$VmDC,
+        [string]$VmFs,
+        [string]$VmTs
     )
     
-    #Die MacAddressen der Vms auslesen
-    $DCMacAddress = Get-VM -Name $DC | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
-    $FSMacAddress = Get-VM -Name $FS | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
-    $TSMacAddress = Get-VM -Name $TS | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
+    $DcMacAddress = Get-VM -Name $VmDC | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
+    $FsMacAddress = Get-VM -Name $VmFs | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
+    $TSsMacAddress = Get-VM -Name $VmTs | Get-VMNetworkAdapter | Select-Object -ExpandProperty MacAddress
 
-    #Den VMs statische MacAddressen geben
-    Set-VMNetworkAdapter -VMName $DC -StaticMacAddress $DCMacAddress
-    Set-VMNetworkAdapter -VMName $FS -StaticMacAddress $FSMacAddress
-    Set-VMNetworkAdapter -VMName $TS -StaticMacAddress $TSMacAddress
+    Set-VMNetworkAdapter -VMName $VmDC -StaticMacAddress $DcMacAddress
+    Set-VMNetworkAdapter -VMName $VmFs -StaticMacAddress $FsMacAddress
+    Set-VMNetworkAdapter -VMName $VmTs -StaticMacAddress $TSsMacAddress
 }
 
-<#Change the IP Addresses of the Windows Server on the VMs to static IP Adresses and install the Fileserver Featuer on the FS VM#>
+<#
+.SYNOPSIS
+Changes IP addresses and hostnames of VMs, and installs the File Server role.
+
+.DESCRIPTION
+Runs PowerShell scripts inside each VM to configure static IP addresses, rename computers, and install the File Server feature on the FS VM.
+
+.PARAMETER VmDC
+Name of the Domain Controller VM.
+
+.PARAMETER VmFs
+Name of the File Server VM.
+
+.PARAMETER VmTs
+Name of the Terminal Server VM.
+
+.PARAMETER Credential
+Credentials used to execute the remote scripts in the VMs.
+#>
 function ChangeVMSettings {
     param(
-        [string]$DC,
-        [string]$FS,
-        [string]$TS,
+        [string]$VmDC,
+        [string]$VmFs,
+        [string]$VmTs,
         [pscredential]$Credential
     )
 
-    #Den VMs eine statische IP zuweisen, sowie den Computernamen aendern
-    Invoke-Command -VMName $DC -FilePath ".\VMHandling\ChangeIP_rename_DC.ps1" -Credential $Credential -AsJob | Out-Null
-    Invoke-Command -VMName $FS -FilePath ".\DeployFileServerRole.ps1" -Credential $Credential -AsJob | Out-Null
-    Invoke-Command -VMName $TS -FilePath ".\VMHandling\ChangeIp_Rename_TS.ps1" -Credential $Credential -AsJob | Out-Null
+    Invoke-Command -VMName $VmDC -FilePath ".\VMHandling\ChangeIP_rename_DC.ps1" -Credential $Credential -AsJob | Out-Null
+    Invoke-Command -VMName $VmFs -FilePath ".\DeployFileServerRole.ps1" -Credential $Credential -AsJob | Out-Null
+    Invoke-Command -VMName $VmTs -FilePath ".\VMHandling\ChangeIp_Rename_TS.ps1" -Credential $Credential -AsJob | Out-Null
+
     Get-Job | Wait-Job | Out-Null
     Get-Job | Where-Object State -like 'Completed' | Remove-Job | Out-Null
 }
