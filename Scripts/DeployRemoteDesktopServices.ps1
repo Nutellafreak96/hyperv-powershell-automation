0284e5c2f622c4086ba4035238002cc4c1864fc4
nutellafreak96 <huebi.kevin@gmx.de>
2025-07-30 15:13:54 +0200

    Add missing collection name to Add-RDSessionHost command

--- Scripts/DeployRemoteDesktopServices.ps1
+++ Scripts/DeployRemoteDesktopServices.ps1
@@ -5,7 +5,7 @@ $FQDN = "TS." + $Using:DomainName
 
 
 
-Add-RDSessionHost -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
+Add-RDSessionHost -SessionHost $FQDN -ConnectionBroker $FQDN -CollectionName $Using:KundeRDP | Out-Null
 New-RDSessionDeployment -ConnectionBroker $FQDN -SessionHost $FQDN -WebAccessServer $FQDN | Out-Null
-New-RDSessionCollection -CollectionName $Using:KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
+#New-RDSessionCollection -CollectionName $Using:KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
  
