2bb6782f02992312c0b2c771a19de924d72db5c2
nutellafreak96 <huebi.kevin@gmx.de>
2025-07-31 15:43:17 +0200

    made some changes to the rds deployment script

--- Scripts/DeployRemoteDesktopServices.ps1
+++ Scripts/DeployRemoteDesktopServices.ps1
@@ -4,8 +4,8 @@
 $FQDN = "TS." + $Using:DomainName
 
 
-
-Add-RDSessionHost -SessionHost $FQDN -ConnectionBroker $FQDN -CollectionName $Using:KundeRDP | Out-Null
+#Install-WindowsFeature -IncludeManagementTools -Name @("Remote-Desktop-Services", "RDS-RD-Server","RDS-Connection-Broker","RDS-Web-Access") -Restart | Out-Null
 New-RDSessionDeployment -ConnectionBroker $FQDN -SessionHost $FQDN -WebAccessServer $FQDN | Out-Null
-#New-RDSessionCollection -CollectionName $Using:KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
+#Add-RDSessionHost -SessionHost $FQDN -ConnectionBroker $FQDN -CollectionName $Using:KundeRDP | Out-Null
+New-RDSessionCollection -CollectionName $Using:KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
  
