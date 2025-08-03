e88160fcc88b5dc162e1720a5c63e5a79d59f673
Nutellafreak <huebi.kevin@gmx.de>
2025-08-03 20:56:01 +0200

    changed script to only install remote desktop roles

--- Scripts/DeployRemoteDesktopServices.ps1
+++ Scripts/DeployRemoteDesktopServices.ps1
@@ -1,11 +1,10 @@
 ﻿#Erstellen eines Terminalservers durch das Installieren der Rolle Remodedesktopservices
 
+Install-WindowsFeature -IncludeManagementTools -Name @( 
+            "Remote-Desktop-Services",
+            "RDS-RD-Server", 
+            "RDS-Connection-Broker", 
+            "RDS-Web-Access",
+            "RDS-Licensing"
+        ) -Restart | Out-Null
 
-$FQDN = "TS." + $Using:DomainName
-
-
-#Install-WindowsFeature -IncludeManagementTools -Name @("Remote-Desktop-Services", "RDS-RD-Server","RDS-Connection-Broker","RDS-Web-Access") -Restart | Out-Null
-New-RDSessionDeployment -ConnectionBroker $FQDN -SessionHost $FQDN -WebAccessServer $FQDN | Out-Null
-#Add-RDSessionHost -SessionHost $FQDN -ConnectionBroker $FQDN -CollectionName $Using:KundeRDP | Out-Null
-New-RDSessionCollection -CollectionName $Using:KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
- 
