af7b001904eca75a4daef407f9ea5ae35cb1493a
nutellafreak96 <huebi.kevin@gmx.de>
2025-08-04 16:08:34 +0200

    tried to configure the rds licens server

--- Scripts/ConfigureRemoteDesktopDeployment.ps1
+++ Scripts/ConfigureRemoteDesktopDeployment.ps1
@@ -6,6 +6,7 @@ param(
 $FQDN = "TS." + $Using:DomainName
 
 New-RDSessionDeployment -ConnectionBroker $FQDN -SessionHost $FQDN -WebAccessServer $FQDN | Out-Null
+Set-RDLicenseConfiguration -LicenseServer $FQDN -Mode PerUser -ConnectionBroker $FQDN -Force | Out-Null
 New-RDSessionCollection -CollectionName $Using:KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
 Set-RDSessionCollectionConfiguration -UserGroup "TSUser" -CollectionName $Using:KundeRDP | Out-Null
 
