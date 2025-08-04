69ee966885ee67ecf3e3ff01754ea263c2516da3
nutellafreak96 <huebi.kevin@gmx.de>
2025-08-04 10:34:43 +0200

    deleted a comment

--- Scripts/ConfigureRemoteDesktopDeployment.ps1
+++ Scripts/ConfigureRemoteDesktopDeployment.ps1
@@ -6,7 +6,6 @@ param(
 $FQDN = "TS." + $Using:DomainName
 
 New-RDSessionDeployment -ConnectionBroker $FQDN -SessionHost $FQDN -WebAccessServer $FQDN | Out-Null
-#Add-RDServer -Server $FQDN  -Role "RDS-RD-SERVER" -ConnectionBroker $FQDN | Out-Null
 New-RDSessionCollection -CollectionName $Using:KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
 Set-RDSessionCollectionConfiguration -UserGroup "TSUser" -CollectionName $Using:KundeRDP | Out-Null
 
