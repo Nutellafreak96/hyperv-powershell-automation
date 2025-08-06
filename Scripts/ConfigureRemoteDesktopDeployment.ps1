2547a14514dde432112d3a9d4e9ba1c13e3d21d0
Nutellafreak <huebi.kevin@gmx.de>
2025-08-06 17:25:45 +0200

    removed the Using parameter

--- Scripts/ConfigureRemoteDesktopDeployment.ps1
+++ Scripts/ConfigureRemoteDesktopDeployment.ps1
@@ -3,10 +3,10 @@ param(
     [string]$DomainName
 )
 
-$FQDN = "TS." + $Using:DomainName
+$FQDN = "TS." + $DomainName
 
 New-RDSessionDeployment -ConnectionBroker $FQDN -SessionHost $FQDN -WebAccessServer $FQDN | Out-Null
 Set-RDLicenseConfiguration -LicenseServer $FQDN -Mode PerUser -ConnectionBroker $FQDN -Force | Out-Null
-New-RDSessionCollection -CollectionName $Using:KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
-Set-RDSessionCollectionConfiguration -UserGroup "TSUser" -CollectionName $Using:KundeRDP | Out-Null
+New-RDSessionCollection -CollectionName $KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
+Set-RDSessionCollectionConfiguration -UserGroup "TSUser" -CollectionName $KundeRDP | Out-Null
 
