d92cbde8af642789608866ceef0f97ceda6218dd
Nutellafreak <huebi.kevin@gmx.de>
2025-08-18 21:21:54 +0200

    removed licensing settings cause of errors

--- Scripts/ConfigureRemoteDesktopDeployment.ps1
+++ Scripts/ConfigureRemoteDesktopDeployment.ps1
@@ -6,7 +6,6 @@ param(
 $FQDN = "TS." + $DomainName
 
 New-RDSessionDeployment -ConnectionBroker $FQDN -SessionHost $FQDN -WebAccessServer $FQDN | Out-Null
-Set-RDLicenseConfiguration -LicenseServer $FQDN -Mode PerUser -ConnectionBroker $FQDN -Force | Out-Null
 New-RDSessionCollection -CollectionName $KundeRDP -SessionHost $FQDN -ConnectionBroker $FQDN | Out-Null
 Set-RDSessionCollectionConfiguration -UserGroup "TSUser" -CollectionName $KundeRDP | Out-Null
 
